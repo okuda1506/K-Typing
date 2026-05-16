7deb335dfec7101be643c106500c3bdd54ba8b20
okuda1506 <okuda1506@gmail.com>
2026-05-16 13:54:14 +0900

    バリデーションルールを定義したDTクラスの作成

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,6 +1,33 @@
+import {
+    IsNotEmpty,
+    IsEmail,
+    IsString,
+    MinLength,
+    MaxLength,
+} from 'class-validator';
+
 export class CreateUserDto {
-    name?: string;
-    displayName?: string;
-    email?: string;
-    password?: string;
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3)
+    @MaxLength(20)
+    name!: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3)
+    @MaxLength(20)
+    displayName!: string;
+
+    @IsEmail()
+    @IsString()
+    @MinLength(5)
+    @MaxLength(50)
+    email!: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    password!: string;
 }

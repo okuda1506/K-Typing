a133e3bff241e25a6c2a2bc027a0d1420ce662d6
okuda1506 <okuda1506@gmail.com>
2026-05-16 13:19:33 +0900

    グローバルバリデーション設定

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // グローバルバリデーション設定
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true, // DTO に定義されていないプロパティを除去
+            forbidNonWhitelisted: true, // DTO に定義されていないプロパティがある場合にエラーとする
+            transform: true, // リクエストデータを DTO クラスのインスタンスに変換
+        }),
+    );
+
     await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();

3acb860d6080790249d026e69b12ae672444f9c2
okuda1506 <okuda1506@gmail.com>
2026-05-23 11:42:44 +0900

    グローバルガード設定

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalGuards(new JwtAuthGuard()); // グローバルガード設定
 
     // グローバルバリデーション設定
     app.useGlobalPipes(

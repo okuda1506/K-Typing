c421858bdcbc51c387e936c64b7f62fab13424f0
okuda1506 <okuda1506@gmail.com>
2026-06-10 07:19:35 +0900

    AuthControllerにsignUp()を実装

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { PublicUser } from '../users/types/public-user.type';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('signup')
+    async signUp(@Body() dto: SignUpDto): Promise<PublicUser> {
+        return this.authService.signUp(dto);
+    }
+}

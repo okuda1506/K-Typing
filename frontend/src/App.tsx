0f0726f5b9ddfe035d7922692a90ede26241a17f
okuda1506 <okuda1506@gmail.com>
2026-06-29 08:21:56 +0900

    sonnerの導入とToasterを使えるようにする実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { AppLayout } from './components/layout/AppLayout'
-import { SignInPage } from './features/auth/SignInPage'
-import { SignUpPage } from './features/auth/SignUpPage'
-import { HomePage } from './features/home/HomePage'
-import { OnboardingPage } from './features/onboarding/OnboardingPage'
-import { ResultPage } from './features/result/ResultPage'
-import { TypingPage } from './features/typing/TypingPage'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AppLayout } from './components/layout/AppLayout';
+import { SignInPage } from './features/auth/SignInPage';
+import { SignUpPage } from './features/auth/SignUpPage';
+import { HomePage } from './features/home/HomePage';
+import { OnboardingPage } from './features/onboarding/OnboardingPage';
+import { ResultPage } from './features/result/ResultPage';
+import { TypingPage } from './features/typing/TypingPage';
+import { Toaster } from '@/components/ui/sonner';
 
 function App() {
     return (
@@ -18,9 +19,10 @@ function App() {
                 <Route path="/lessons/:lessonId/typing" element={<TypingPage />} />
                 <Route path="/sessions/:sessionId/result" element={<ResultPage />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
+                <Toaster />
             </Routes>
         </AppLayout>
-    )
+    );
 }
 
-export default App
+export default App;
